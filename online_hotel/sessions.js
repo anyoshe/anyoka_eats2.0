cd893e9cad30218dffe3b50f4111572017121182
anyoshe <anyokaayoka@gmail.com>
2024-09-22 20:07:43 +0300

    adjusting the ordering system of the fresh food orders

--- online_hotel/sessions.js
+++ online_hotel/sessions.js
@@ -3,5 +3,4 @@ const crypto = require('crypto');
 // Generate a 64-byte random string and convert it to hexadecimal format
 const secretKey = crypto.randomBytes(64).toString('hex');
 
-console.log('Generated JWT Secret Key:', secretKey);
-
+node -e "console.log(require('crypto').randomBytes(64).toString('hex'))"
