3d768030610d8455437c948def246971e3a82a1a
wisdom52 <pro1totype@gmail.com>
2024-12-13 16:16:50 +0300

    landing page changes

--- client/src/components/Landing/LandingTestimonial.js
+++ client/src/components/Landing/LandingTestimonial.js
@@ -32,9 +32,9 @@ function Testimonials() {
           nisi ut aliquip ex ea commodo consequat.
           </p>
 
-          <img
+          {/* <img
           src="https://images.unsplash.com/photo-1595152452543-e5fc28ebc2b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
-          />
+          /> */}
 
           <h3 className="name">Albert Sinelly</h3>
 
@@ -51,9 +51,9 @@ function Testimonials() {
           nisi ut aliquip ex ea commodo consequat.
           </p>
 
-          <img
+          {/* <img
           src="https://images.unsplash.com/photo-1627541718143-1adc1b582e62?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8bXVzbGltfGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-          />
+          /> */}
 
           <h3 className="name">Hirok Meryam</h3>
 
@@ -69,9 +69,9 @@ function Testimonials() {
             nisi ut aliquip ex ea commodo consequat.
             </p>
 
-            <img
+            {/* <img
             src="https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fGZhY2V8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-            />
+            /> */}
 
             <h3 className="name">Sebastian Sert</h3>
 
@@ -88,9 +88,9 @@ function Testimonials() {
           nisi ut aliquip ex ea commodo consequat.
           </p>
 
-          <img
+          {/* <img
           src="https://images.unsplash.com/photo-1595152452543-e5fc28ebc2b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
-          />
+          /> */}
 
           <h3 className="name">Albert Sinelly</h3>
 
@@ -106,9 +106,9 @@ function Testimonials() {
           nisi ut aliquip ex ea commodo consequat.
           </p>
 
-          <img
+          {/* <img
           src="https://images.unsplash.com/photo-1627541718143-1adc1b582e62?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8bXVzbGltfGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-          />
+          /> */}
 
           <h3 className="name">Hirok Meryam</h3>
 
@@ -124,9 +124,9 @@ function Testimonials() {
             nisi ut aliquip ex ea commodo consequat.
             </p>
 
-            <img
+            {/* <img
             src="https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fGZhY2V8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-            />
+            /> */}
 
             <h3 className="name">Sebastian Sert</h3>
 
@@ -142,9 +142,9 @@ function Testimonials() {
           nisi ut aliquip ex ea commodo consequat.
           </p>
 
-          <img
+          {/* <img
           src="https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fGZhY2V8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-          />
+          /> */}
 
           <h3 className="name">Sebastian Sert</h3>
 
